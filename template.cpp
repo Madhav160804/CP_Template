dbe07b60e67ac8f6278e8424e128f28df5d44766
Madhav Dhingra <madhavdhingra11@gmail.com>
2024-01-28 19:25:52 +0530

    added template

---
+++ template.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+ 
+#define vint vector<int>
+#define take(a,n) vint a(n); for(int j=0;j<n;j++) cin>>a[j];
+#define give(a,n) for(int j=0;j<n;j++) cout<<a[j]<<' ';
+#define pb push_back
+#define r0 return 0
+#define f0(i,n) for(int i=0; i<(int)n; ++i)
+#define f1(i,n) for(int i=1; i<=(int)n; ++i)
+#define nl '\n'
+#define MOD 1000000007
+#define pii pair<int,int>
+#define test int t;cin>>t;while(t--)
+#define int long long
+#define mp make_pair
+#define sz(a) a.size()
+#define be(a) a.begin(),a.end()
+using namespace std;
+ 
+//////////////////////////////////////////////////////////////
+ 
+void solve(){
+    
+}
+ 
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+ 
+    test
+        solve();
+ 
+    return 0;
+}
